c4cc86170899440fb84b804fe311d587a0300d8b
Alex Gabriel Araujo <104734641+AlexGabAraujo@users.noreply.github.com>
2025-06-27 03:52:34 -0300

    BANCO

---
+++ banco.sql
@@ -0,0 +1,12 @@
+create database cu;
+use cu;
+
+create table Pessoa(
+	Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	Nome varchar(255) NOT NULL,
+    EMAIL VARCHAR(255) NOT NULL,
+    Senha VARCHAR(255) NOT NULL
+);
+    
+    
+select * from pessoa;
